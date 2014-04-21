390fbeeec91ea2f19ff7a86a0b8ea4fa89340ce4
Armin Pašalić <armin@pasalic.com.ba>
2014-04-21 12:55:43 +0200

    Fix definition, write explanation for 1.4

--- chapter_1/1.4.scm
+++ chapter_1/1.4.scm
@@ -3,6 +3,15 @@
 ; are compound expressions. Use this observation to describe the behavior of
 ; the following procedure:
 ;
-;   (define (a-plus-abs-b a b)
-;     (if (> b 0) + -) a b)
+
+   (define (a-plus-abs-b a b)
+     ((if (> b 0) + -) a b))
+
+; If b is greater than 0, apply '+' to the operands. Otherwise apply '-'.
+;
+; In Ruby this is written similar to:
+;
+;    def a_plus_abs_b(a, b)
+;      a.send(b > 0 ? :+ : :-, b)
+;    end
 ;
