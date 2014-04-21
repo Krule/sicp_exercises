d2255dfd2ac09429bfd1bc21169d71b41fde01bf
Armin Pašalić <armin@pasalic.com.ba>
2014-04-21 12:25:44 +0200

    Remove examples from 1.3

--- chapter_1/1.3.scm
+++ chapter_1/1.3.scm
@@ -13,13 +13,3 @@
   (cond ((and (<= a b) (<= a c)) (²sum b c))
         ((<= b c) (²sum a c))
         (else (²sum a b))))
-
-(²sum-largest-two 1 2 3); 13
-(²sum-largest-two 3 2 1); 13
-(²sum-largest-two 3 1 2); 13
-(²sum-largest-two 2 3 1); 13
-(²sum-largest-two 1 3 2); 13
-(²sum-largest-two 1 3 2); 13
-(²sum-largest-two 2 2 1);  8
-(²sum-largest-two 1 2 2);  8
-(²sum-largest-two 2 2 2);  8
